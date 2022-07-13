e560eafa598691e2c6c5cb58b88e5650462fd575
ahmadhusain0205 <ahmad.husain.ardiansyah11@gmail.com>
2022-07-13 19:49:46 +0700

    setting kondisi for save perencanaan

--- application/controllers/Perencanaan.php
+++ application/controllers/Perencanaan.php
@@ -31,4 +31,41 @@ class Perencanaan extends CI_Controller
           $data = $this->db->get_where('subprogram', ['kodeprog' => $kodeprog])->result();
           echo json_encode($data);
      }
+     public function save_header()
+     {
+          $data = array(
+               'standar_pendidikan' => $this->input->post('standar_pendidikan'),
+               'nama_kegiatan' => $this->input->post('nama_kegiatan'),
+               'program' => $this->input->post('program'),
+               'sub_program' => $this->input->post('sub_program'),
+               'triwulan' => $this->input->post('triwulan'),
+               'subtotal' => $this->input->get('subtotal'),
+               'total' => $this->input->get('total'),
+          );
+
+          // $this->M_perencanaan->save($data);
+          // echo json_encode(['status' => 1]);
+          echo json_encode($data);
+     }
+     public function save_detail()
+     {
+          $get_id = $this->db->query("SELECT id FROM perencanaan order by id desc limit 1")->result();
+          foreach ($get_id as $gi) {
+               $id_perencanaan = $gi->id;
+          }
+          $namabarang = $this->input->get('namabarang');
+          $satuan = $this->input->get('satuan');
+          $qty = $this->input->get('qty');
+          $harga = $this->input->get('harga');
+          $jumlah = $this->input->get('jumlah');
+          $data = [
+               'id_perencanaan' => $id_perencanaan,
+               'namabarang' => $namabarang,
+               'satuan' => $satuan,
+               'qty' => $qty,
+               'harga' => $harga,
+               'jumlah' => $jumlah,
+          ];
+          $this->db->insert('perencanaan_uraian', $data);
+     }
 }
--- application/views/Perencanaan/Tambah.php
+++ application/views/Perencanaan/Tambah.php
@@ -8,7 +8,7 @@
                          </a>
                     </div>
                     <hr>
-                    <form method="POST" id="form-tambah">
+                    <form method="POST" id="form_tambah">
                          <div class="row">
                               <div class="col">
                                    <div class="form-group row">
@@ -29,6 +29,91 @@
                                              <select name="subprogram" id="subprogram" class="form-control select2_subprog"></select>
                                         </div>
                                    </div>
+                                   <div class="form-group row">
+                                        <label for="triwulan" class="col-sm-2 col-form-label">Triwulan</label>
+                                        <div class="col-sm-5">
+                                             <select name="triwulan" id="triwulan" class="form-control select2_triwulan">
+                                                  <option value="1">Ke 1 (satu)</option>
+                                                  <option value="2">Ke 2 (dua)</option>
+                                                  <option value="3">Ke 3 (tiga)</option>
+                                                  <option value="4">Ke 4 (empat)</option>
+                                             </select>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                         <hr>
+                         <div class="h6">URAIAN</div>
+                         <div class="row">
+                              <div class="col">
+                                   <div class="table-responsive">
+                                        <table id="datatable" class="table table-striped table-bordered table-hover">
+                                             <thead>
+                                                  <tr class="text-center bg-primary text-white">
+                                                       <th>Delete</th>
+                                                       <th width="20%">Nama Barang</th>
+                                                       <th>Satuan</th>
+                                                       <th width="10%">Volume</th>
+                                                       <th>Harga</th>
+                                                       <th>Jumlah</th>
+                                                  </tr>
+                                             </thead>
+                                             <tbody>
+                                                  <tr>
+                                                       <td>
+                                                            <button type='button' onclick="hapusBarisIni(1)" class='btn btn-danger btn-sm btn-circle' id="del1"><i class='fa fa-trash'></i>
+                                                       </td>
+                                                       <td>
+                                                            <input name="namabarang[]" id="namabarang1" type="text" class="form-control">
+                                                       </td>
+                                                       <td>
+                                                            <input name="satuan[]" id="satuan1" type="text" class="form-control">
+                                                       </td>
+                                                       <td>
+                                                            <input name="qty[]" onchange="totalline(1);qtyc(1)" value="1" id="qty1" type="text" class="form-control">
+                                                       </td>
+                                                       <td>
+                                                            <input name="harga[]" onchange="totalline(1); cekharga(1);" value="0" id="harga1" type="text" class="form-control">
+                                                       </td>
+                                                       <td>
+                                                            <input name="jumlah[]" id="jumlah1" type="text" class="form-control" size="40%" readonly value="0">
+                                                       </td>
+                                                  </tr>
+                                             </tbody>
+                                        </table>
+                                   </div>
+                                   <div class="row">
+                                        <div class="col">
+                                             <button type="button" onclick="tambah()" class="btn btn-primary btn-sm btn-circle" style="margin-left: 10px;">
+                                                  <i class="fa fa-plus"></i>
+                                             </button>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="row">
+                              <div class="col-3 offset-9">
+                                   <div class="row g-3 align-items-center mb-3">
+                                        <div class="col-4">
+                                             <label for="sub_total" class="col-form-label">Sub total</label>
+                                        </div>
+                                        <div class="col-8">
+                                             <input type="text" id="sub_total" name="sub_total" class="form-control" readonly value="0">
+                                        </div>
+                                   </div>
+                                   <div class="row g-3 align-items-center mb-3">
+                                        <div class="col-4">
+                                             <label for="total" class="col-form-label">Total</label>
+                                        </div>
+                                        <div class="col-8">
+                                             <input type="text" id="total" name="total" class="form-control" readonly value="0">
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="row justify-content-center">
+                              <div class="col-3 offset-9">
+                                   <button type="button" onclick="save()" class="btn btn-info btn-sm float-right"><i class="fas fa-save"></i> Simpan</button>
                               </div>
                          </div>
                     </form>
@@ -40,6 +125,7 @@
 <script>
      $(window).on("load", function() {
           $('.select2_subprog').attr('disabled', true);
+          $('.select2_triwulan').select2();
           initailizeSelect2_subprog(null);
      });
 
@@ -47,5 +133,211 @@
           var prog = $("#program").val();
           initailizeSelect2_subprog(prog);
           $('.select2_subprog').attr('disabled', false);
+     }
+</script>
+
+<!-- master -->
+<script>
+     function separateComma(val) {
+          var sign = 1;
+          if (val < 0) {
+               sign = -1;
+               val = -val;
+          }
+          let num = val.toString().includes('.') ? val.toString().split('.')[0] : val.toString();
+          let len = num.toString().length;
+          let result = '';
+          let count = 1;
+          for (let i = len - 1; i >= 0; i--) {
+               result = num.toString()[i] + result;
+               if (count % 3 === 0 && count !== 0 && i !== 0) {
+                    result = ',' + result;
+               }
+               count++;
+          }
+          if (val.toString().includes('.')) {
+               result = result + '.' + val.toString().split('.')[1];
+          }
+          return sign < 0 ? '-' + result : result;
+     }
+     var arr = [1];
+     var idrow = 2;
+     if (idrow == 2) {
+          $('#del1').attr('disabled', true);
+     }
+</script>
+
+<script>
+     function tambah() {
+          var table = document.getElementById('datatable');
+          rowCount = table.rows.length;
+          arr.push(idrow);
+          var x = document.getElementById('datatable').insertRow(rowCount);
+          var td1 = x.insertCell(0);
+          var td2 = x.insertCell(1);
+          var td3 = x.insertCell(2);
+          var td4 = x.insertCell(3);
+          var td5 = x.insertCell(4);
+          var td6 = x.insertCell(5);
+          var button = "<td id='kolom" + idrow + "'><button type='button' onclick=hapusBarisIni(" + idrow + ") id=del" + idrow + " class='btn btn-danger btn-sm btn-circle'><i class='fa fa-trash'></td>";
+          var barang = "<td><input name='namabarang[]' id='namabarang" + idrow + "' class='form-control'></td>";
+          var satuan = "<td><input name='satuan[]' id='satuan" + idrow + "' class='form-control'></td>";
+          var qty = "<td><input name='qty[]' id=qty" + idrow + " onchange='totalline(" + idrow + "); qtyc(" + idrow + ")' value=1  type='text' class='form-control rightJustified'></td>";
+          var harga = "<td><input name='harga[]'  id=harga" + idrow + " onchange='totalline(" + idrow + ");cekharga(" + idrow + ");' value='0'  type='text' class='form-control rightJustified'> </td>";
+          var jum = "<td><input name='jumlah[]' id=jumlah" + idrow + " type='text' class='form-control rightJustified' size='40%' readonly value='0'></td>";
+          td1.innerHTML = button;
+          td2.innerHTML = barang;
+          td3.innerHTML = satuan;
+          td4.innerHTML = qty;
+          td5.innerHTML = harga;
+          td6.innerHTML = jum;
+          idrow++;
+     }
+
+     function hapusBarisIni(param) {
+          var x = document.getElementById('datatable').deleteRow(arr.indexOf(param) + 1);
+          arr.splice(arr.indexOf(param), 1);
+          rowCount--;
+          total();
+     }
+</script>
+
+<!-- aritmatika rp -->
+<script>
+     function cekharga(id) {
+          var harga = $('#harga' + id).val();
+          $('#harga' + id).val(separateComma(harga));
+     }
+
+     function qtyc(id) {
+          var qty = $('#qty' + id).val();
+          $('#qty' + id).val(separateComma(qty));
+     }
+
+     function totalline(id) {
+          var table = document.getElementById('datatable');
+          var row = table.rows[arr.indexOf(id) + 1];
+          var hargax = parseInt(row.cells[4].children[0].value.replaceAll(',', ''));
+          var jumlah = parseInt(row.cells[3].children[0].value.replaceAll(',', '')) * hargax;
+          var subtot = jumlah;
+          var tot = jumlah;
+          row.cells[5].children[0].value = separateComma(tot);
+          total();
+     }
+
+     function total() {
+          var table = document.getElementById('datatable');
+          var rowCount = table.rows.length;
+          tjumlah = 0;
+          tppn = 0;
+          for (var i = 1; i < rowCount; i++) {
+               var row = table.rows[i];
+               qty = row.cells[3].children[0].value;
+               harga = row.cells[4].children[0].value;
+               jumlah = row.cells[5].children[0].value;
+               var qty1 = Number(qty.replace(/[^0-9\.]+/g, ""));
+               var harga1 = Number(harga.replace(/[^0-9\.]+/g, ""));
+               var jumlah1 = Number(jumlah.replace(/[^0-9\.]+/g, ""));
+               tjumlah = tjumlah + (qty1 * harga1);
+          }
+          document.getElementById("sub_total").value = separateComma(tjumlah);
+          document.getElementById("total").value = separateComma(tjumlah + tppn);
+     }
+</script>
+
+<!-- simpan -->
+<script>
+     function save() {
+          var standar_pendidikan = document.getElementById('standar_pendidikan').value;
+          var nama_kegiatan = document.getElementById('nama_kegiatan').value;
+          var program = document.getElementById('program').value;
+          var sub_program = document.getElementById('sub_program').value;
+          var triwulan = document.getElementById('triwulan').value;
+          var sub_totalx = document.getElementById('sub_total').value;
+          var sub_total = Number(parseInt(sub_totalx.replaceAll(',', '')));
+          var totalx = document.getElementById('total').value;
+          var total = Number(parseInt(totalx.replaceAll(',', '')));
+
+          if (standar_pendidikan == '') {
+               Swal.fire({
+                    icon: 'danger',
+                    title: 'Standar Pendidikan',
+                    text: 'Tidak boleh kosong !',
+               });
+          }
+          if (nama_kegiatan == '') {
+               Swal.fire({
+                    icon: 'danger',
+                    title: 'Nama Kegiatan',
+                    text: 'Tidak boleh kosong !',
+               });
+          }
+          if (program == '') {
+               Swal.fire({
+                    icon: 'danger',
+                    title: 'Program',
+                    text: 'Tidak boleh kosong !',
+               });
+          }
+          if (sub_program == '') {
+               Swal.fire({
+                    icon: 'danger',
+                    title: 'Sub Program',
+                    text: 'Tidak boleh kosong !',
+               });
+          }
+          if (triwulan == '') {
+               Swal.fire({
+                    icon: 'danger',
+                    title: 'Triwulan',
+                    text: 'Tidak boleh kosong !',
+               });
+          }
+
+          if (standar_pendidikan != '' && nama_kegiatan != '' && program != '' && sub_program != '' && triwulan != '') {
+
+               $.ajax({
+                    url: "<?php echo base_url(); ?>Perencanaan/save_header/?subtotal=" + sub_total + '&total=' + total,
+                    type: "POST",
+                    data: $("#form_tambah").serialize(),
+                    dataType: "JSON",
+                    success: function(data) {
+                         console.log(data)
+                         // var table = document.getElementById('datatable');
+                         // rowCount = table.rows.length;
+                         // arr.push(idrow);
+                         // for (i = 1; i < rowCount; i++) {
+                         //      var namabarang = $('#namabarang' + i).val();
+                         //      var satuan = $('#satuan' + i).val();
+                         //      var qtyx = $('#qty' + i).val();
+                         //      var hargax = $('#harga' + i).val();
+                         //      var jumlahx = $('#jumlah' + i).val();
+                         //      var qty = parseInt(qtyx.replaceAll(',', ''));
+                         //      var harga = parseInt(hargax.replaceAll(',', ''));
+                         //      var jumlah = parseInt(jumlahx.replaceAll(',', ''));
+                         //      $.ajax({
+                         //           url: '<?= site_url() ?>Perencanaan/save_detail/?namabarang=' + namabarang + '&satuan=' + satuan + '&qty=' + qty + '&harga=' + harga + '&jumlah=' + jumlah,
+                         //           type: 'GET',
+                         //           dataType: 'JSON',
+                         //      });
+                         // }
+                         // if (data.status == 1) {
+                         //      Swal.fire({
+                         //           icon: 'success',
+                         //           title: 'Sukses',
+                         //           text: 'Save Data Berhasil !',
+                         //      }).then((value) => {
+                         //           location.href = "<?php echo base_url() ?>Perencanaan";
+                         //      });
+                         // } else {
+                         //      Swal.fire({
+                         //           icon: 'danger',
+                         //           title: 'Gagal',
+                         //           text: 'Save Data Gagal !',
+                         //      });
+                         // }
+                    }
+               });
+          }
      }
 </script>
